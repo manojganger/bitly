e12fe6bd20d87bbd888493b0be95ae40992f7383
manojganger <manojganger@yahoo.com>
2023-11-20 11:33:46 -0800

    v1

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ This script loads two files and join it together to get the long_url and the cli
 Dups are checked and it gets warned in pytest. De-duping is not performed.
 
 The two files are in json and csv format but are loaded into a pandas dataframe and then merged
-on the hash value. The hash value is extracted and two different methods are used to ensure that it alerts in case specs changes.
+on the hash value. The hash value is extracted and two different methods are used to ensure that
+it alerts in case specs changes.
 
 Analysis is performed on the merged dataframe.
 
@@ -13,12 +14,9 @@ The results are displayed on to the stdout.
 
 """
 
-import pandas as pd
-import json
 import time
 import configparser
-import sys
-import os
+import pandas as pd
 
 def parse_for_hash(short_url):
     """This method parse the hash value from the shortened url
@@ -26,14 +24,16 @@ def parse_for_hash(short_url):
         Parameters
         ----------
         short_url : str
-            The short url which contains hash value. It is assumed that it would be 7 char long but if it changes, it would be detected by pytest
+            The short url which contains hash value. It is assumed that it would be 7 char long but
+            if it changes, it would be detected by pytest
 
         Returns
         -------
         rightmost 7 chars of short_url
             
     """
-    return short_url[-7:]   #could be different so use s[s.rfind('/')+1:len(s)]
+    # could be different so use s[s.rfind('/')+1:len(s)]
+    return short_url[-7:]
 
 def read_config():
     """This method reads configuration setting in config/config.ini
@@ -41,7 +41,6 @@ def read_config():
         Parameters
         ----------
         None
-
 
         Returns
         -------
@@ -78,11 +77,11 @@ def read_datafiles(val):
     data_list_decode = []
     data_list_encode = []
 
-    with open(file_path_decode, 'r') as file:
+    with open(file_path_decode, 'r', encoding="utf-8") as file:
         for chunk in pd.read_json(file, lines=True, chunksize=chunk_size):
             data_list_decode.extend(chunk.to_dict(orient='records'))
 
-    with open(file_path_encode, 'r') as file:
+    with open(file_path_encode, 'r', encoding="utf-8") as file:
         for chunk in pd.read_csv(file,  chunksize=chunk_size):
             data_list_encode.extend(chunk.to_dict(orient='records'))
 
@@ -125,7 +124,7 @@ def analyse_datafiles(df_decode, df_encode):
     print(df_decode.head())
 
 
-    #join the df on hash and get the 
+    #join the df on hash and get the
     res = pd.merge(df_decode,df_encode, on='hash', how='left')
     print(res[['bitlink', 'hash', 'long_url']])
 
@@ -134,7 +133,7 @@ def analyse_datafiles(df_decode, df_encode):
 
     return res
 
-def display_results(res):
+def display_results(df_merged):
     """This method displays the output on console as per the format
 
             Parameters
@@ -150,24 +149,25 @@ def display_results(res):
     """
 
     #groupby long_url and get the counts
-    res1=res[['long_url']].value_counts().reset_index(name='counts')
-    #convert the dataframe to list of dict 
-    print([(dict(zip(res1.long_url, res1.counts)))])
+    df_clicks = df_merged[['long_url']].value_counts().reset_index(name='counts')
+
+    #convert the dataframe to list of dict
+    print([(dict(zip(df_clicks.long_url, df_clicks.counts)))])
 
     #filter only rows belonging to 2021
-    df_2021 = res[(res['dt'] >= '2021-01-01') & (res['dt'] <= '2021-12-31')]
-    res3 = df_2021[['long_url']].value_counts().reset_index(name='counts')
+    df_2021 = df_merged[(df_merged['dt'] >= '2021-01-01') & (df_merged['dt'] <= '2021-12-31')]
+    df_clicks_2021 = df_2021[['long_url']].value_counts().reset_index(name='counts')
 
-    print([(dict(zip(res3.long_url, res3.counts)))])
+    print([(dict(zip(df_clicks_2021.long_url, df_clicks_2021.counts)))])
 
 
 if __name__ == '__main__':
+
     #time the whole process
     start = time.time()
-    config_dict = read_config()
-    df_decode, df_encode = read_datafiles(config_dict)
-    df_merged = analyse_datafiles(df_decode, df_encode)
-    display_results(df_merged)
+    config_data = read_config()
+    df_decode_data, df_encode_data = read_datafiles(config_data)
+    df_merged_data = analyse_datafiles(df_decode_data, df_encode_data)
+    display_results(df_merged_data)
 
     print(f"\n\nThe process took {round(time.time()-start,2)} secs")
-
